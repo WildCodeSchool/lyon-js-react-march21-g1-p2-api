ba9c95ad8362e19923b7b2016fe564f60dfa86f6
Cécile Dubois <duboiscecilepro@gmail.com>
2021-04-27 17:31:22 +0200

    Added links to get-post request between the app and api

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const cors = require('cors');
+const Joi = require('joi');
 const { PORT, CORS_ALLOWED_ORIGINS, inTestEnv } = require('./env');
+const connection = require('./db-config');
 
 const app = express();
+
+app.use(express.json());
+
+connection.connect((err) => {
+  if (err) {
+    console.error('error connecting to db');
+  } else {
+    console.log('connected to db');
+  }
+});
 
 // app settings
 app.set('x-powered-by', false); // for security
@@ -21,14 +33,62 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-// example for test
-const ingredients = [
-  { name: 'Tomato', quantity: 5 },
-  { name: 'Olives', quantity: 2 },
-];
+const orderRouter = express.Router();
+app.use('/orders', orderRouter);
 
-app.get('/ingredients', (req, res) => {
-  res.json(ingredients);
+orderRouter.get('/', (req, res) => {
+  connection
+    .promise()
+    .query('SELECT * FROM orders')
+    .then(([results]) => {
+      res.json(results);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+});
+
+orderRouter.post('/', (req, res) => {
+  const { ingredients, quantity, price } = req.body;
+  const { error: validationErrors } = Joi.object({
+    ingredients: Joi.string().max(255).required(),
+    quantity: Joi.number().integer().min(0).required(),
+    price: Joi.number().min(0).precision(2).required(),
+  }).validate({ ingredients, quantity, price }, { abortEarly: false });
+
+  if (validationErrors) {
+    res.status(422).send({ validationErrors });
+  } else {
+    connection
+      .promise()
+      .query(
+        'INSERT INTO orders (ingredients, quantity, price) VALUES (?, ?, ?)',
+        [ingredients, quantity, price]
+      )
+      .then(([result]) => {
+        res.send({ id: result.insertId, ingredients, quantity, price });
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
+  }
+});
+
+orderRouter.get('/:id', (req, res) => {
+  const { id } = req.params;
+  connection
+    .promise()
+    .query('SELECT * FROM orders WHERE id = ?', [id])
+    .then(([results]) => {
+      if (results.length) res.send(results[0]);
+      else res.sendStatus(404);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
 });
 
 // server setup
