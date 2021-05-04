8d869b866a3bc750641d8164835fe2c4ca5b0351
Cécile Dubois <duboiscecilepro@gmail.com>
2021-05-04 15:08:08 +0200

    Button 'Commander' of PredefinedFoodPage now directly record the order of this pizza

--- pizzas.sql
+++ pizzas.sql
@@ -1,14 +1,17 @@
 USE upizz;
 CREATE TABLE pizzas (
   pizzas_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  pizzas_name VARCHAR(255) NOT NULL,
+  pizzas_userid VARCHAR(255) NOT NULL,
+  pizzas_name VARCHAR(100) NOT NULL,
   pizzas_imgaddress VARCHAR(255) NOT NULL,
   pizzas_ingredients VARCHAR(255) NOT NULL,
-  pizzas_price FLOAT(10, 2) CHECK(pizza_price >= 0) NOT NULL,
-  pizzas_kcal INT CHECK(pizza_kcal >= 0) NOT NULL
+  pizzas_price FLOAT(10, 2) CHECK(pizzas_price >= 0) NOT NULL,
+  pizzas_kcal INT CHECK(pizzas_kcal >= 0) NOT NULL
 );
+
 INSERT INTO
   pizzas (
+    pizzas_userid,
     pizzas_name,
     pizzas_imgaddress,
     pizzas_ingredients,
@@ -17,23 +20,26 @@ INSERT INTO
   )
 VALUES
   (
+    'admin',
     'Margarita',
     'margarita.png',
-    'Tomates, Mozzarella, Olives, Roquette',
-    9,
-    0
+    '{"Pâte à pizza":1,"Sauce tomate":1,"Tomates":3,"Mozzarella":1,"Olives":3,"Roquette":2}',
+    10.5,
+    947
   ),
   (
+    'admin',
     'Regina',
     'regina.png',
-    'Tomates, Fromage, Jambon, Champignons, Olives, Roquette',
-    12,
-    0
+    '{"Pâte à pizza":1,"Sauce tomate":1,"Tomates":2,"Fromage":1,"Jambon":1,"Champignons":1,"Olives":2,"Roquette":2}',
+    10.5,
+    974
   ),
   (
+    'admin',
     'Diavola',
     'diavola.png',
-    'Tomates, Fromage, Chorizo, Oignons, Poivrons',
+    '{"Pâte à pizza":1,"Sauce tomate":1,"Tomates":3,"Fromage":1,"Chorizo":2,"Oignons":2,"Poivrons":2}',
     11,
-    0
+    1177
   ); 
